5db310a5d67a3c4ee8ab0d0105a2b35489e29b68
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2023-12-27 05:04:17 +0000

    some fixes

---
+++ deploy/scripts/update-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+docker stop $1
+docker rm $1
+
+docker-compose -f ${SCRIPT_DIR}/../apps/docker-compose.$2.yml up -d
