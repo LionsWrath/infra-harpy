5820fcdea0047e3313231829ca3e8a48d43b3274
Caio Henrique Segawa Tonetti <caio.tonetti-c@newsela.com>
2024-01-11 18:07:25 -0300

    some corrections

--- deploy/scripts/route-vpn.sh
+++ deploy/scripts/route-vpn.sh
@@ -1,3 +1,3 @@
 docker exec --privileged qbittorrent ip route del default
-docker exec --privileged qbittorrent ip route add default via 172.16.0.2
+docker exec --privileged qbittorrent ip route add default via 172.16.0.50
 docker exec --privileged qbittorrent ip route add 172.19.0.0/16 via 172.16.0.1
