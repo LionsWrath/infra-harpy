6052bcc284a74b272dc154499abdbb21b784f2e7
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-01-12 00:12:04 -0300

    changing some dns configs

--- deploy/scripts/route-vpn.sh
+++ deploy/scripts/route-vpn.sh
@@ -1,3 +1,5 @@
+docker exec --privileged 
+
 docker exec --privileged qbittorrent ip route del default
 docker exec --privileged qbittorrent ip route add default via 172.16.0.50
 docker exec --privileged qbittorrent ip route add 172.19.0.0/16 via 172.16.0.1
