857fa0d0ee626522fa96e193f1415b9bc540b875
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-01-19 00:51:06 -0300

    changing some configs

--- deploy/scripts/route-vpn.sh
+++ deploy/scripts/route-vpn.sh
@@ -1,2 +1,3 @@
 docker exec --privileged qbittorrent ip route del default
 docker exec --privileged qbittorrent ip route add default via 172.19.0.50
+docker exec --privileged qbittorrent ip route add 100.64.0.0/10 via 172.19.0.1
