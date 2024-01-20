65d3aaa6a90ef8cff3725e3951c7e1398a9f0d08
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-01-20 13:30:15 -0300

    improvements for the vpn scripts

--- deploy/scripts/route-vpn.sh
+++ deploy/scripts/route-vpn.sh
@@ -1,6 +1,28 @@
 #############################################################
 # Add all containers to be secured here                     #
-# They need to have the secure.sh mapped internally         #
+#############################################################
+# Route configuration                                       #
+#-----------------------------------------------------------#
+# WG      = 172.19.0.50                                     #
+# GATEWAY = 172.19.0.1                                      #
+# TAILNET = 100.64.0.0/10                                   #
 #############################################################
 
-docker exec qbittorrent sh -c /scripts/secure.sh
+declare -a arr=(
+    "qbittorrent"
+    "sonarr"
+    "radarr"
+    "bazarr"
+    "jackett"
+)
+
+for i in "${arr[@]}"
+do
+    echo -n "($i): "
+
+    docker exec --privileged $i ip route del default
+    docker exec --privileged $i ip route add default via 172.19.0.50
+    docker exec --privileged $i ip route add 100.64.0.0/10 via 172.19.0.1
+
+    docker exec $i curl -s https://am.i.mullvad.net/connected
+done
