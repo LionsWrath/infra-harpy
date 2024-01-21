b4fa09da1ede587fc1055fb2a2397e748383e8d7
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-01-21 02:17:13 -0300

    new service and some improvements

--- deploy/scripts/route-vpn.sh
+++ deploy/scripts/route-vpn.sh
@@ -20,9 +20,13 @@ for i in "${arr[@]}"
 do
     echo -n "($i): "
 
-    docker exec --privileged $i ip route del default
-    docker exec --privileged $i ip route add default via 172.19.0.50
-    docker exec --privileged $i ip route add 100.64.0.0/10 via 172.19.0.1
+    if [ "$( docker container inspect -f '{{.State.Running}}' $i )" = "true" ]; then
+        docker exec --privileged $i ip route del default
+        docker exec --privileged $i ip route add default via 172.19.0.50
+        docker exec --privileged $i ip route add 100.64.0.0/10 via 172.19.0.1
 
-    docker exec $i curl -s https://am.i.mullvad.net/connected
+        docker exec $i curl -s https://am.i.mullvad.net/connected
+    else
+        echo "container not running!"
+    fi
 done
