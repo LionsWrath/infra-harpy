a96e8f37ebb3e7324f231ae442c04eb710d6277f
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-01-23 01:34:19 -0300

    some good updates to scripts

--- deploy/scripts/internal/ntfy_test.sh
+++ deploy/scripts/internal/ntfy_test.sh
@@ -12,12 +12,15 @@
 #  rotating_light
 #  computer
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/../vars.sh
+
 notify () {
     docker exec nginx-proxy-manager curl \
         -H "Title: Test Notification" \
         -H "Priority: $1" \
         -H "Tags: $2" \
-        -d $3 \
+        -d "$3" \
         $4
 }
 
