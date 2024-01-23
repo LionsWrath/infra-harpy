05885f6e281832196e0449bc2243c8fec15a0f88
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-01-23 01:58:20 -0300

    one edge case was missing

--- deploy/scripts/internal/db_backup.sh
+++ deploy/scripts/internal/db_backup.sh
@@ -8,6 +8,11 @@ if [ "$( docker container inspect -f '{{.State.Running}}' postgres )" = "true" ]
     FILENAME=$(date +"%Y_%m_%d")_pg_bck.gz
     FILEPATH=${DATA_PATH}/backups/postgresql
 
+    if [ ! -f ${FILEPATH}/${FILENAME} ]; then
+        notify "DB Backup Process" "high" "warning" "File ${FILENAME} already exists. Exiting." ${MANAGE_TOPIC}
+        exit
+    fi
+
     docker exec postgres pg_dumpall -U harpydb | gzip > ${FILEPATH}/${FILENAME}
 
     if [ ! -f ${FILEPATH}/${FILENAME} ]; then
