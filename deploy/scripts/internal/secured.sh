b0fbf6e93f3233f974aca73d6b90e3f10e2fe701
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-01-23 22:02:33 -0300

    some new improvements

--- deploy/scripts/internal/secured.sh
+++ deploy/scripts/internal/secured.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+check_container_vpn () {
+    CHECK=$(docker exec $1 curl -s https://am.i.mullvad.net/connected)
+
+    if [[ $CHECK == *"You are not connected to Mullvad."* ]]; then
+        echo "false"
+    else
+        echo "true"
+    fi
+}
+
 declare -a arr=(
     "qbittorrent"
     "sonarr"
--- deploy/scripts/route-vpn.sh
+++ deploy/scripts/route-vpn.sh
@@ -8,19 +8,20 @@
 # TAILNET = 100.64.0.0/10                                   #
 #############################################################
 
-declare -a arr=(
-    "qbittorrent"
-    "sonarr"
-    "radarr"
-    "bazarr"
-    "jackett"
-)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/internal/secured.sh
 
 for i in "${arr[@]}"
 do
     echo -n "($i): "
 
     if [ "$( docker container inspect -f '{{.State.Running}}' $i )" = "true" ]; then
+
+        if [ "$( check_container_vpn $i )" = "true" ]; then
+            echo "already connected!"
+            continue
+        fi
+
         docker exec --privileged $i ip route del default
         docker exec --privileged $i ip route add default via 172.19.0.50
         docker exec --privileged $i ip route add 100.64.0.0/10 via 172.19.0.1
