500b57f48e43b89e41f0781b4a14be01207a5993
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-02-10 19:47:39 +0000

    some adjustments

--- deploy/scripts/internal/bck_culling.sh
+++ deploy/scripts/internal/bck_culling.sh
@@ -3,14 +3,21 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/../vars.sh
 
+BACKUP_PATH=${DATA_PATH}/backups/postgresql
 RETENTION_TIME=30
 
-arr1=$(ls -t | head -n $RETENTION_TIME)
-arr2=$(ls -t)
+arr1=$(ls -t ${BACKUP_PATH} | head -n $RETENTION_TIME)
+arr2=$(ls -t ${BACKUP_PATH})
 
-res=$(${arr1[@]} ${arr2[@]} | tr ' ' '\n' | sort | uniq -u)
+if (( ${#arr1[@]} == 0)); then
+    echo "No file to cull."
+else
+    line=$(echo ${arr1[@]} ${arr2[@]} | tr ' ' '\n' | sort | uniq -u)
+    readarray -t res <<< "$line"
 
-for i in "${res[@]}"
+    for i in "${res[@]}"
     do
-        rm ${DATA_PATH}/backups/postgresql/$i
+        echo ${BACKUP_PATH}/$i
     done
+
+fi
