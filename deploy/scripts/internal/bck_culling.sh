b84a9b8043037e72bbb6768d7fc45bce9fd97e0f
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-02-09 21:44:53 -0300

    culling algorithm

--- deploy/scripts/internal/bck_culling.sh
+++ deploy/scripts/internal/bck_culling.sh
@@ -1,1 +1,16 @@
-#TODO
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/../vars.sh
+
+RETENTION_TIME=30
+
+arr1=$(ls -t | head -n $RETENTION_TIME)
+arr2=$(ls -t)
+
+res=$(${arr1[@]} ${arr2[@]} | tr ' ' '\n' | sort | uniq -u)
+
+for i in "${res[@]}"
+    do
+        rm ${DATA_PATH}/backups/postgresql/$i
+    done
