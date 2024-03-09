9fb77148ec631ee8995f3b9a27b1b4bd97f6bc9e
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-03-08 22:18:45 -0300

    some observation

--- deploy/scripts/internal/s3_sync.sh
+++ deploy/scripts/internal/s3_sync.sh
@@ -5,6 +5,8 @@
 # the machine!                                              #
 #############################################################
 
+# This is not tested yet!
+
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
