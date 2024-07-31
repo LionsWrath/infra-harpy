80c18300cd8f253a4e0ab3025ad2b4e95d07c2ec
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2024-07-30 23:57:00 -0300

    fixing stuff again

--- deploy/scripts/vars.sh
+++ deploy/scripts/vars.sh
@@ -1,4 +1,5 @@
 export DATA_PATH=${HOME}/data
 export SOURCE_PATH=${HOME}/terraform/deploy
-export MANAGE_TOPIC=ntfy:8088/manage
+export NTFY_INTERNAL_ADDRESS=ntfy:8088
+export MANAGE_TOPIC=${NTFY_INTERNAL_ADDRESS}/manage
 export S3_PATH=s3://harpy-dl
